7b32c3e40fb206bb634b4593dbef13cf5cbb1bee
VeeranganaMalav <veeranganamalav01@gmail.com>
2022-05-29 17:49:17 +0530

    initial commit

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "root@1998",
+    DB: "testdb",
+    dialect: "mysql"
+};
---
+++ models/index.js
@@ -0,0 +1,15 @@
+const dbConfig = require("../config/config.js");
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false
+});
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.tutorials = require("./tutorial.js")(sequelize, Sequelize);
+
+module.exports = db;
